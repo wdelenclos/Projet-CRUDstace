f404e9a850e3b61bf600478e3c15967bc5476b60
Yann Le Scouarnec <Ikimasoft@users.noreply.github.com>
2016-05-24 10:02:44 +0200

    Commentaires et squelette de nav (#18)

--- view/404.php
+++ view/404.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <title>404</title>
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
 </head>
 <body role="document">
 <div class="container theme-showcase" role="main">
     <div class="jumbotron">
-        <h1>HAHAHHAH T PERDU</h1>
+        <h1>Page non trouv&eacute;e</h1>
     </div>
 </div>
 </body>
