59966235fc730633dd3c2b66c8984cefb460b6c5
Wladimir Delenclos <wdelenclos@gmail.com>
2016-05-24 10:24:15 +0200

    display

--- view/page-display.php
+++ view/page-display.php
@@ -7,10 +7,7 @@
     <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
 </head>
 <body role="document">
-<<<<<<< HEAD
-=======
 <?=$nav?>
->>>>>>> f404e9a850e3b61bf600478e3c15967bc5476b60
 <div class="container theme-showcase" role="main">
 <?=$page->body?>
 </div>
