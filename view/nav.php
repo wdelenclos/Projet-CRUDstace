dd1dcc957e69eb1155715e0e7af76503eacb99b9
Wladimir Delenclos <wdelenclos@gmail.com>
2016-05-24 10:14:08 +0200

    nav

--- view/nav.php
+++ view/nav.php
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-<nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.html">WtfWeb</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="?p=teletubbies">Teletubbies</a></li>
-                    <li><a href="?p=kittens">Kittens</a></li>
-                    <li><a href="?p=ironmaiden">Iron Maiden</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-=======
+
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
@@ -28,4 +13,4 @@
         </div>
     </div>
 </nav>
->>>>>>> f404e9a850e3b61bf600478e3c15967bc5476b60
+
